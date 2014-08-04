0293d43ebeb50135da91e73ef88733768ddd70be
Ciaran Downey <me@ciarand.me>
2014-08-03 17:45:10 -0700

    Add first attempt at lua/bob

---
+++ lua/bob/bob.lua
@@ -0,0 +1,17 @@
+local hey = function(str)
+    -- it's silence if it's an empty string
+    if str == "" then return "Fine, be that way." end
+
+    -- it's a shout if it's all upcase
+    if str:upper() == str then return "Woah, chill out!" end
+
+    -- it's a question if the last character is ?
+    if str:sub(-1) == "?" then return "Sure" end
+
+    -- everything else is w/e
+    return "Whatever"
+end
+
+return {
+    hey = hey
+}
