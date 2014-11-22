bd12935be93c722791eecf4b5f94499585cd43af
Ciaran Downey <me@ciarand.me>
2014-11-22 15:01:52 -0800

    Extract the yelling check to a method in bob.exs

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,23 +1,20 @@
-# Bob is a lackadaisical teenager. In conversation, his responses are very
-# limited.
 defmodule Teenager do
   def hey(input) do
     cond do
-      # it's rude to say hey with silence
-      String.strip(input) == ""
-      -> "Fine. Be that way!"
-
-      # it's a question if the last character is a question mark
-      String.at(input, -1) == "?"
-      -> "Sure."
-
-      # it's only yelling if it's ALL CAPS and there are characters that can be
-      # capitalized (i.e. if you can notice a difference after downcasing it)
-      input == String.upcase(input) and input != String.downcase(input)
-      -> "Woah, chill out!"
-
-      true
-      -> "Whatever."
+      # a silent greeting makes a Teenager mad
+      String.strip(input) == ""   -> "Fine. Be that way!"
+      # Teenagers are not good at answering questions
+      String.at(input, -1) == "?" -> "Sure."
+      # Teenagers don't like being yelled at
+      is_yelling?(input)          -> "Woah, chill out!"
+      # Teenagers are apathetic toward all other things
+      true                        -> "Whatever."
     end
+  end
+
+  # it's only yelling if it's ALL CAPS and there are characters that can be
+  # capitalized (i.e. if you can notice a difference after downcasing it)
+  defp is_yelling?(input) do
+    input === String.upcase(input) and input !== String.downcase(input)
   end
 end
