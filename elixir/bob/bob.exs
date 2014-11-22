33be8215e7914b087a4bcd65e03a14d76d97589f
Ciaran Downey <me@ciarand.me>
2014-11-22 01:28:31 -0800

    Add Bob exercise in Elixir

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,7 +1,23 @@
+# Bob is a lackadaisical teenager. In conversation, his responses are very
+# limited.
 defmodule Teenager do
   def hey(input) do
     cond do
+      # it's rude to say hey with silence
+      String.strip(input) == ""
+      -> "Fine. Be that way!"
 
+      # it's a question if the last character is a question mark
+      String.at(input, -1) == "?"
+      -> "Sure."
+
+      # it's only yelling if it's ALL CAPS and there are characters that can be
+      # capitalized (i.e. if you can notice a difference after downcasing it)
+      input == String.upcase(input) and input != String.downcase(input)
+      -> "Woah, chill out!"
+
+      true
+      -> "Whatever."
     end
   end
 end
