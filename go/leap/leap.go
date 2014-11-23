a8b2b992906c2bc322056068dff7c0657291b4b2
Ciaran Downey <me@ciarand.me>
2014-11-22 23:14:36 -0800

    Cleanup leap.go using a switch statement
    Man Go is so cool

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,18 +1,19 @@
 package leap
 
 // IsLeapYear returns a boolean indicating whether the provided year is a leap
-// year
+// year.
 func IsLeapYear(year int) bool {
+	switch {
 	// if the year isn't a multiple of 4, it's not a leap year
-	if year%4 != 0 {
+	case year%4 != 0:
 		return false
-	}
 
-	// if it's divisible by 400, it's a leap year
-	if year%400 == 0 {
+	// if it's divisible by 400, it's definitely a leap year
+	case year%400 == 0:
 		return true
-	}
 
-	// otherwise it's a leap year as long as it's not divisible by 100
-	return !(year%100 == 0)
+	// otherwise it's a leap year unless it's divisible by 100
+	default:
+		return year%100 != 0
+	}
 }
